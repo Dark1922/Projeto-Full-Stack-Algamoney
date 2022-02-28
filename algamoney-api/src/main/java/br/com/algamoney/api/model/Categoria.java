db84e1a412826d55e756d8d0c18d7e8b9fbc7504
dark1922 <jvdematos004@gmail.com>
2022-02-28 20:20:01 -0300

    feath: criando model e repository.

---
+++ algamoney-api/src/main/java/br/com/algamoney/api/model/Categoria.java
@@ -0,0 +1,26 @@
+package br.com.algamoney.api.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
+	private Long id;
+	
+	private Long nome;
+
+}
