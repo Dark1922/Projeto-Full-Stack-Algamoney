732a7c24ef351251b3b67834c8c93a62c90569a1
dark1922 <jvdematos004@gmail.com>
2022-02-28 19:46:19 -0300

    feath: criando projeto backend.

---
+++ algamoney-api/src/test/java/br/com/algamoney/api/AlgamoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.algamoney.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgamoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
