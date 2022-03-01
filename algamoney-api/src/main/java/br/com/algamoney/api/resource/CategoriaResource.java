c186745007bff6aaa7abf841eb86c29e1b8dd2e5
dark1922 <jvdematos004@gmail.com>
2022-02-28 22:46:26 -0300

    feath: tratando erros que não existem em nossa model.

--- algamoney-api/src/main/java/br/com/algamoney/api/resource/CategoriaResource.java
+++ algamoney-api/src/main/java/br/com/algamoney/api/resource/CategoriaResource.java
@@ -1,14 +1,20 @@
 package br.com.algamoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.algamoney.api.model.Categoria;
 import br.com.algamoney.api.repository.CategoriaRepository;
@@ -20,15 +26,26 @@ import lombok.RequiredArgsConstructor;
 public class CategoriaResource {
 
 	private final CategoriaRepository categoriaRepository;
-	
+
 	@GetMapping
-	public ResponseEntity<List<Categoria>>  findAll() {
-		return  ResponseEntity.ok(categoriaRepository.findAll());
+	public ResponseEntity<List<Categoria>> findAll() {
+		return ResponseEntity.ok(categoriaRepository.findAll());
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<Categoria> saveCategoria(@RequestBody Categoria caregoria) {
-		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaRepository.save(caregoria));
+	public ResponseEntity<Categoria> saveCategoria(@RequestBody Categoria caregoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(caregoria);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+				.buildAndExpand(categoriaSalva.getId()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long id) {
+		  Optional<Categoria> categoria = categoriaRepository.findById(id);
+	        return categoria.isPresent() ? ResponseEntity.ok(categoria.get()) : ResponseEntity.notFound().build();
 	}
 }
-	
